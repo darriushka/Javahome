bc16b6f6eabc8eb429e176aa4c6e78ccfa7b542b
darriushka <dariagolub1@gmail.com>
2022-09-25 15:09:12 +0300

    sol

--- polina/src/main/java/Task1.java
+++ polina/src/main/java/Task1.java
@@ -1,25 +1,35 @@
-public class Task1
-{
+public class Task1 {
     //посчитать факториал числа n
     //0.5 балла - если посчитаете в цикле
     //1 балл - если посчитаете рекурсией
-    public static int fact(int n){
-        return n;
+    public static int fact(int n) {
+        if (n == 1)
+            return 1;
+        return n * fact(n - 1);
     }
 
     //вывести таблицу умножения на экран - 1 балл
     //подсказка - использовать двойной for
-    public static void table(){
+    public static void table() {
         //Ваше решение здесь
+        for(int i=1; i<10;i++)
+            for(int j=1; j<10;j++)
+                System.out.println(i+"*"+j+"="+i*j);
+
     }
 
     //посчитать сумму цифр числа
     //можно посчитать для трехзначного - 0.5 балла
     //для любого числа - 1 балл
     //подсказка - в случае для любого числа используйте while
-    public static int sum(int n){
+    public static int sum(int n) {
         //Ваше решение здесь
-        return n;
+        int s=0;
+        while (n!=0) {
+            s += (n % 10);
+            n=n/10;
+        }
+        return s;
     }
 
     //определить, является ли год високосным
@@ -31,33 +41,57 @@ public class Task1
     //за правильный ответ - 0.5 балла
     public static boolean isLeapYear(int year) {
         //Ваше решение здесь
-        return true;
+        if (year %100==0 && year %400==0)
+            return true;
+        else if(year %4==0 && year %100!=0)
+            return true;
+        else if(year %100==0)
+            return false;
+        else
+            return false;
     }
 
     //здесь вам нужно будет использовать результат прошлой задачи
     //и вывести, сколько дней в году
     //правильный ответ - 0.5 балла
     public static int daysInYear(int year) {
-        if (isLeapYear(year)){
-            //
+        if (isLeapYear(year)) {
+            return 366;
         } else {
-            //
+            return 365;
         }
-        return 0;
     }
 
     //определить номер дня недели по строке
     //например: Понедельник - 1
     //правильный ответ - 1 балл
-    public static int dayOfTheWeek(String n){
+    public static int dayOfTheWeek(String n) {
         //Ваше решение здесь
+        if (n.equals("Понедельник"))
+            return 1;
+        if (n.equals("Вторник"))
+            return 2;
+        if (n.equals("Среда"))
+            return 3;
+        if (n.equals("Четверг"))
+            return 4;
+        if (n.equals("Пятница"))
+            return 5;
+        if (n.equals("Суббота"))
+            return 6;
+        if (n.equals("Воскресенье"))
+            return 7;
         return 0;
     }
 
     //вывести массив на экран в виде: [1, 5, 3, 7, 10, 2, 5]
     //правильное решение - 0.5 балла
-    public static void printArray(int[] array){
+    public static void printArray(int[] array) {
         //Ваше решение здесь
+              /*  for (int i=0;i<array.length;i++){
+                    System.out.print(array[i]+" ");
+                }*/
+
     }
 
     //вывести двойной массив на экран в виде:
@@ -65,22 +99,42 @@ public class Task1
     // [1, 5, 3, 7, 10, 2, 5]
     // ...
     //правильное решение - 0.5 балла
-    public static void printArray(int[][] array){
+    public static void printArray(int[][] array) {
         //Ваше решение здесь
+        for (int i=0;i<array.length;i++){
+            for (int j=0;j<array[i].length;j++){
+                System.out.print(array[i][j]+" ");
+            }
+        }
     }
 
     //отсортировать одномерный массив в порядке возрастания
     //если не знаете, как сортировать, то подсказка -
     //метод пузырька (один из самых простых для понимания)
     //правильный ответ - 1 балл
-    public static int[] sort(int[] array){
+    public static int[] sort(int[] array) {
         //Ваше решение здесь
+        boolean s=false;
+        while(!s){
+            s=true;
+            for (int i=0;i<(array.length-1);i++){
+                if (array[i]>array[i+1]){
+                    int x=array[i];
+                    array[i]=array[i+1];
+                    array[i+1]=x;
+                    s=false;
+                }
+            }
+        }
+        for (int i=0;i<array.length;i++){
+            System.out.print(array[i]+" ");
+        }
         return array;
     }
 
 
     //здесь можете тестировать свои решения
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Факториал:");
         System.out.println(fact(5));
 
@@ -96,12 +150,12 @@ public class Task1
         System.out.println("День недели:");
         System.out.println(dayOfTheWeek("Понедельник"));
 
-        int[] array1D = {1,5,3,7,10,2,5};
+        int[] array1D = {1, 5, 3, 7, 10, 2, 5};
         System.out.println("Вывод отсортированного массива:");
         printArray(sort(array1D));
 
         System.out.println("Вывод двумерного массива:");
-        int[][] array2D = {{1,5,3,7,10,2,5}, {1,5,3,7,10,2,5}};
+        int[][] array2D = {{1, 5, 3, 7, 10, 2, 5}, {1, 5, 3, 7, 10, 2, 5}};
         printArray(array2D);
     }
 }
